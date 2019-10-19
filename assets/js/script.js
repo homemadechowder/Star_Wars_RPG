3a8a99f7374f7d4b31bbacd2cba20eba44564994
Andrew Lu <awclu@ucdavis.edu>
2019-10-18 21:04:28 -0700

    added new rows with new items

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,81 @@ $(document).ready(function() {
 
 
     var isCharSelected = false;
-
-    $(".charBox").on("click", function(){
-
-    console.log("yoda clicked");
+    var charName = "";
     
-    isCharSelected = true;
+    //Hide all the not needed portions, this part definitely has a more elegant way but CTRLC CTRLV works.
+    $("#luke2").hide();
+    $("#obi2").hide();
+    $("#yoda2").hide();
+    $("#qui2").hide();
+    $("#luke3").hide();
+    $("#obi3").hide();
+    $("#yoda3").hide();
+    $("#qui3").hide();
 
+    $("#luke").on("click", function(){
+
+    charName = "Luke Skywalker"    
+    isCharSelected = true;
+    
     charSelection();
     
-    });
+    $("#luke2").show();
+    $("#obi3").show();
+    $("#yoda3").show();
+    $("#qui3").show();
     
+    });
+    $("#obi").on("click", function(){
+
+        charName = "Obi-Wan Kenobi"    
+        isCharSelected = true;
+    
+        charSelection();
+    
+        $("#obi2").show();
+        $("#luke3").show();
+        $("#yoda3").show();
+        $("#qui3").show();
+    
+    });
+    $("#yoda").on("click", function(){
+
+        charName = "Jedi Grandmaster Yoda"    
+        isCharSelected = true;
+        
+        charSelection();
+        $("#yoda2").show();
+        $("#obi3").show();
+        $("#yoda3").show();
+        $("#qui3").show();
+        
+    });
+    $("#qui").on("click", function(){
+
+        charName = "Qui-Gonn Jinn the Grey Jedi"    
+        isCharSelected = true;
+        
+        charSelection();
+        $("#qui2").show();
+        $("#obi3").show();
+        $("#yoda3").show();
+        $("#luke3").show();
+        
+ });
     console.log(isCharSelected)
     function charSelection(){
+      
+        console.log($(this).text());
         $("#charRow").hide();
-        console.log($(this).val());
+        
         if (isCharSelected){
-            $("#charSelect").html("Your Character: ");
+            $("#charSelect").html("Your Character is " + charName);
         }
         
-    }
+        $("#enemies").text("Enemies Available To Attack ");
+        $("#fightSection").text("Fight Section");
+        $("").text("Enemies Available To Attack: ");
+        $("#defender").text("Defender is ");   
+     }
 })
